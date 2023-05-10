fc50af2a17431e10a525b32cd46eea43cda0d643
moidunovTimur <moidunovtimur@gmail.com>
2023-05-10 18:50:17 +0600

    first commit

--- app/src/main/java/com/example/lovecalculator/ui/MainActivity.kt
+++ app/src/main/java/com/example/lovecalculator/ui/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lovecalculator.databinding.ActivityMainBinding
 
-
+// LoveCalculator
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
